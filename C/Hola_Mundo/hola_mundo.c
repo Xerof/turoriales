8dbe5d87f69882d318e5857b8e3c11009daa984d
Xerof <fercho27@gmail.com>
2016-05-05 21:46:23 -0500

    Primer Programa: Hola Mundo
    Signed-off-by: Xerof <fercho27@gmail.com>

---
+++ C/Hola_Mundo/hola_mundo.c
@@ -0,0 +1,9 @@
+#incluide <stdio.h> /* Agregamos la libreria donde se encuentra definida la funcion printf
+					   printf nos sirve para imprimir en pantalla */
+
+int main ( ) /* esta es nuestra funcion principal, nuestro punto de entrada de nuestro programa */
+{ 
+	printf("Hola Mundo\n"); /* imprimimos en pantalla el mensaje Hola Mundo */
+
+	return 0; /* Indicamos que nuestro programa termino con exito */
+}
